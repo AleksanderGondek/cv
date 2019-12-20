b82a631cf412209194d5cfcd776f641ef3974b0a
Aleksander Gondek <gondekaleksander@protonmail.com>
2019-12-20 15:12:42 +0100

    First draft of the CV

--- default.nix
+++ default.nix
@@ -1,31 +1,34 @@
 { pkgs ? import <nixpkgs> {}, ... }:
 
-pkgs.stdenv.mkDerivation {
-  name = "agondek-cv";
-  src = ./.;
+let
+  userSecrets = import ./userSecrets.nix;
+in
+  pkgs.stdenv.mkDerivation({
+    name = "agondek-cv";
+    src = ./.;
 
-  buildInputs = with pkgs; [ 
-    (texlive.combine {
-      inherit (texlive)
-        scheme-medium
-        fontawesome
-        geometry
-        hyperref
-        moresize
-        raleway;
-    })
-  ];
+    buildInputs = with pkgs; [ 
+      (texlive.combine {
+        inherit (texlive)
+          scheme-medium
+          fontawesome
+          geometry
+          hyperref
+          moresize
+          raleway;
+      })
+    ];
 
-  buildPhase = ''
-    # See: https://tex.stackexchange.com/questions/496275/texlive-2019-lualatex-doesnt-compile-document
-    # Without export, lualatex fails silently, with exit code '0'
-    export TEXMFVAR=$(pwd)
-    lualatex main.tex
-  '';
+    buildPhase = ''
+      # See: https://tex.stackexchange.com/questions/496275/texlive-2019-lualatex-doesnt-compile-document
+      # Without export, lualatex fails silently, with exit code '0'
+      export TEXMFVAR=$(pwd)
+      lualatex -interaction=nonstopmode agondek-cv.tex
+    '';
 
-  installPhase = ''
-    mkdir -p $out
-    cp main.log $out
-    cp main.pdf $out
-  '';
-}
+    installPhase = ''
+      mkdir -p $out
+      cp agondek-cv.log $out
+      cp agondek-cv.pdf $out
+    '';
+  } // userSecrets)
